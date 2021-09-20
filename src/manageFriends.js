6c05bc981ced26828d13609ca1309eeb37ae3c67
Joe Christensen <joechristensenwork@gmail.com>
2021-09-20 17:15:54 -0600

    commit tree f0531fffc1b83fd72b77ce461a868870846004e6

--- src/manageFriends.js
+++ src/manageFriends.js
@@ -1,3 +1,21 @@
 export function manageFriends(state, action) {
-  // your code here
+  switch (action.type) {
+    case 'friends/add':
+      return {
+        friends: [
+          ...state.friends,
+          {
+            name: action.payload.name,
+            hometown: action.payload.hometown,
+            id: action.payload.id
+          }
+        ]
+      }
+    case 'friends/remove':
+      return {
+        friends: state.friends.filter((friend) => friend.id !== action.payload)
+      }
+    default:
+      return state
+  }
 }
